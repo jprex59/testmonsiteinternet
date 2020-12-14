f234a1010571fd3325900bc63dc5a608b236217e
jprex59 <76010227+jprex59@users.noreply.github.com>
2020-12-14 23:20:02 +0100

    v1

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+const nav = document.querySelector("nav");
+const backdrop = document.querySelector(".backdrop");
+
+button.addEventListener("click", () => {
+  nav.classList.add("open");
+});
+
+backdrop.addEventListener("click", () => {
+  nav.classList.remove("open");
+});

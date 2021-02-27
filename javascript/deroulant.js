0b3abfff0b2da79ead40e620b4a0339d558ab3fe
jprex59 <76010227+jprex59@users.noreply.github.com>
2021-02-27 10:34:19 +0100

    nav bar permanent

--- javascript/deroulant.js
+++ javascript/deroulant.js
@@ -4,13 +4,8 @@ const iconspans2 = document.querySelectorAll('.closehamburger2 span');  // retou
 const sidebar = document.querySelector('.sidebar');
 const closesidebar = document.querySelector('.closehamburger1');
 const closesidebar2 = document.querySelector('.closehamburger2');
-const containerstage = document.querySelector('.containerstage');
-const prevpharmagest = document.querySelector('.prevpharmagest');
-const nextpharmagest = document.querySelector('.nextpharmagest');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const body = document.querySelector('body');
-const timeline = document.querySelector('.history-tl-container');
+
+
 
 
 let touchStart, touchEnd;
@@ -61,24 +56,12 @@ icon.addEventListener('click', () => {
   closesidebar.classList.add('open');
   icon.style.display = 'none';
   closesidebar2.style.display = 'block';
-  containerstage.style.position = 'initial';
-  prev.style.display = 'none';
-  next.style.display = 'none';
-  prevpharmagest.style.display = 'none';
-  nextpharmagest.style.display = 'none';
-  timeline.style.display = 'none';
 });
 
 closesidebar.addEventListener('click', () => {
   sidebar.classList.remove('open');
   icon.style.display = 'block';
   closesidebar.classList.remove('open');
-  containerstage.style.position = 'relative';
-  prev.style.display = 'block';
-  next.style.display = 'block';
-  prevpharmagest.style.display = 'block';
-  nextpharmagest.style.display = 'block';
-  timeline.style.display = 'block';
 });
 
 closesidebar2.addEventListener('click', () => {
@@ -86,12 +69,6 @@ closesidebar2.addEventListener('click', () => {
   icon.style.display = 'block';
   closesidebar2.style.display = 'none';
   closesidebar.classList.remove('open');
-  containerstage.style.position = 'relative';
-  prev.style.display = 'block';
-  next.style.display = 'block';
-  prevpharmagest.style.display = 'block';
-  nextpharmagest.style.display = 'block';
-  timeline.style.display = 'block';
 });
 
 /**********************************Ouverture au slide tentative************************************/
@@ -115,12 +92,6 @@ body.addEventListener('touchend', a => {
     closesidebar.classList.add('open');
     icon.style.display = 'none';
     closesidebar2.style.display = 'block';
-    containerstage.style.position = 'initial';
-    prev.style.display = 'none';
-    next.style.display = 'none';
-    prevpharmagest.style.display = 'none';
-    nextpharmagest.style.display = 'none';
-    timeline.style.display = 'none';
   }
 });
 
@@ -143,11 +114,5 @@ body.addEventListener('touchend', e => {
     icon.style.display = 'block';
     closesidebar2.style.display = 'none';
     closesidebar.classList.remove('open');
-    containerstage.style.position = 'relative';
-    prev.style.display = 'block';
-    next.style.display = 'block';
-    prevpharmagest.style.display = 'block';
-    nextpharmagest.style.display = 'block';
-    timeline.style.display = 'block';
   }
 });

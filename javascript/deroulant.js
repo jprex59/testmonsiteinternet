b9e31b1d5781cc253387ec3cd73e092826f44649
jprex59 <76010227+jprex59@users.noreply.github.com>
2021-02-27 10:00:56 +0100

    sticky menu

--- javascript/deroulant.js
+++ javascript/deroulant.js
@@ -1,4 +1,6 @@
 const icon = document.querySelector('.hamburger');
+const iconspans = document.querySelectorAll('.hamburger span');  // retourne un tableau
+const iconspans2 = document.querySelectorAll('.closehamburger2 span');  // retourne un tableau
 const sidebar = document.querySelector('.sidebar');
 const closesidebar = document.querySelector('.closehamburger1');
 const closesidebar2 = document.querySelector('.closehamburger2');
@@ -13,6 +15,44 @@ const timeline = document.querySelector('.history-tl-container');
 
 let touchStart, touchEnd;
 let touchStartt, touchEndd;
+
+
+/***********************sticky***************/
+// detecter lorsqu'on scroll
+window.onscroll = function() { sticky() };
+
+// recuperer les elements
+let stickycomponent = icon.offsetTop;
+
+// faire la fonction
+function sticky() {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > stickycomponent) {
+        icon.classList.add("sticky");
+        closesidebar2.classList.add("sticky");
+
+        iconspans.forEach(function(item) {
+          item.style.backgroundColor = 'black';
+      });
+
+        iconspans2.forEach(function(item2) {
+        item2.style.backgroundColor = 'black';
+      });
+    }
+    else{
+        icon.classList.remove("sticky");
+        closesidebar2.classList.remove("sticky");
+
+        iconspans.forEach(function(item) {
+          item.style.backgroundColor = 'white';
+      });
+
+        iconspans2.forEach(function(item2) {
+        item2.style.backgroundColor = 'white';
+      });
+    }
+}
+
 
 /***************************************Enlever les elements qui font buguer quand je clic pour ouvrir le menu****************************************** */
 

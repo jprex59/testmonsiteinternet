49ed4791814b3027a5025c7f8d19044eff826b32
jprex59 <76010227+jprex59@users.noreply.github.com>
2021-03-25 20:39:07 +0100

    Update counter.js

--- javascript/counter.js
+++ javascript/counter.js
@@ -2,7 +2,7 @@ const counter = document.getElementById('counter')
 
 
 const updateCounter = async () => {
-    const data =  await fetch('https://api.countapi.xyz/hit/JeanParis-projet-monsiteinternet/visits')
+    const data =  await fetch('https://api.countapi.xyz/hit/JeanParis-projet-monSiteinternet/visits')
     const count = await data.json();
     /*console.log(count);*/
     counter.innerHTML = count.value;

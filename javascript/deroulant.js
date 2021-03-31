1884995f2d7b28ebd1430d8521e0213d77d5b4d8
jprex59 <76010227+jprex59@users.noreply.github.com>
2021-03-31 23:01:27 +0200

    new menu, design veilles, debug

--- javascript/deroulant.js
+++ javascript/deroulant.js
@@ -1,10 +1,49 @@
+const indicator = document.querySelector('.nav-indicator');
+const items = document.querySelectorAll('.nav-item');
+
+function handleIndicator(el) {
+    // boucler sur tous item -> retirer la calss "is-active"
+    items.forEach(item => {
+        item.classList.remove('is-active');
+        item.removeAttribute('style');
+    })
+
+    // styliser l'indicator 
+    indicator.style.width = `${el.offsetWidth}px`;
+    indicator.style.backgroundColor = '#151728';
+    indicator.style.left = `${el.offsetLeft}px`;
+
+    //ajout de la classe is-active
+    el.classList.add('is-active');
+    el.style.color = '#151728';
+
+}
+
+
+
+items.forEach((item, index ) =>{
+    item.addEventListener('click', (e) => {
+        handleIndicator(e.target)
+    });
+    item.classList.contains('is-active') && handleIndicator(item);
+});
+
+
+width = document.documentElement.clientWidth;
+
+/***************************Deroulant*********************/
 const icon = document.querySelector('.hamburger');
+const icon1 = document.querySelector('.hamburger1');
+const closehamburger_Responsive = document.querySelector('.closehamburger_Responsive');
+const iconspansResponsive = document.querySelectorAll('.closehamburger_Responsive span');  // retourne un tableau
 const iconspans = document.querySelectorAll('.hamburger span');  // retourne un tableau
+const iconspans1 = document.querySelectorAll('.hamburger1 span');  // retourne un tableau
 const iconspans2 = document.querySelectorAll('.closehamburger2 span');  // retourne un tableau
 const sidebar = document.querySelector('.sidebar');
 const closesidebar = document.querySelector('.closehamburger1');
 const closesidebar2 = document.querySelector('.closehamburger2');
 const body = document.querySelector('body');
+const menu = document.querySelector('.menu');
 
 width = document.documentElement.clientWidth;
 console.log(width);
@@ -14,26 +53,54 @@ console.log("width");
 let touchStart, touchEnd;
 let touchStartt, touchEndd;
 
-/***********************sticky***************/
-// detecter lorsqu'on scroll
-window.onscroll = function() { sticky() };
+/******************************************** */
+if (width < 930) {
+    icon1.style.display = 'block';
+    menu.style.display = 'none';
+    closesidebar2.style.display = 'none';
 
-// recuperer les elements
-let stickycomponent = icon.offsetTop;
+    
+icon1.addEventListener('click', () => {
+    sidebar.classList.add('open');
+    closesidebar.classList.add('open');
+    closehamburger_Responsive.style.display = 'block';
+    icon1.style.display = 'none';
+  });
 
-// faire la fonction
-function sticky() {
+  closehamburger_Responsive.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    closesidebar.classList.remove('open');
+    closehamburger_Responsive.style.display = 'none';
+    icon1.style.display = 'block';
+  });
+
+  closesidebar.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    icon1.style.display = 'block';
+    closesidebar.classList.remove('open');
+    closehamburger_Responsive.style.display = 'none';
+  });
+}else{
+    /***********************sticky***************/
+    // detecter lorsqu'on scroll
+    window.onscroll = function() { sticky() };
+
+    // recuperer les elements
+    let stickycomponent = icon.offsetTop;
+
+    // faire la fonction
+    function sticky() {
     console.log(window.pageYOffset);
     if (window.pageYOffset > stickycomponent) {
         icon.classList.add("sticky");
         closesidebar2.classList.add("sticky");
 
         iconspans.forEach(function(item) {
-          item.style.backgroundColor = 'black';
+          item.style.backgroundColor = '#151728';
       });
 
         iconspans2.forEach(function(item2) {
-        item2.style.backgroundColor = 'black';
+        item2.style.backgroundColor = '#151728';
       });
     }
     else{
@@ -48,34 +115,51 @@ function sticky() {
         item2.style.backgroundColor = 'white';
       });
     }
+    }
+
+    function redhamburger(){
+    iconspans.forEach(function(item) {
+        item.style.backgroundColor = '#151728';
+    });
+    }  
+
+    function hamburger(){
+    iconspans.forEach(function(item) {
+        item.style.backgroundColor = '#83818c';
+    });
 }
-
-
 
 /***************************************Enlever les elements qui font buguer quand je clic pour ouvrir le menu****************************************** */
 
 icon.addEventListener('click', () => {
-  sidebar.classList.add('open');
-  closesidebar.classList.add('open');
-  icon.style.display = 'none';
-  closesidebar2.style.display = 'block';
-});
+    sidebar.classList.add('open');
+    closesidebar.classList.add('open');
+    icon.style.display = 'none';
+    menu.style.display = 'none';
+    closesidebar2.style.display = 'block';
+  });
+  
+  closesidebar.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    icon.style.display = 'block';
+    menu.style.display = 'flex';
+    closesidebar.classList.remove('open');
+    closesidebar2.style.display = 'none';
+  });
+  
+  closesidebar2.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    icon.style.display = 'block';
+    menu.style.display = 'flex';
+    closesidebar2.style.display = 'none';
+    closesidebar.classList.remove('open');
+  });
+}
 
-closesidebar.addEventListener('click', () => {
-  sidebar.classList.remove('open');
-  icon.style.display = 'block';
-  closesidebar.classList.remove('open');
-});
 
-closesidebar2.addEventListener('click', () => {
-  sidebar.classList.remove('open');
-  icon.style.display = 'block';
-  closesidebar2.style.display = 'none';
-  closesidebar.classList.remove('open');
-});
 
 /**********************************Ouverture au slide tentative************************************/
-
+/*
 body.addEventListener('touchstart', a => {
   touchStartt = a.targetTouches[0].clientX;
   console.log(touchStartt);
@@ -100,15 +184,15 @@ body.addEventListener('touchend', a => {
 
 
 
-/**************************************************Le slide********************************************* */
+
 body.addEventListener('touchstart', e => {
   touchStart = e.targetTouches[0].clientX;
-  /*console.log(touchStart)*/
+
 });
 
 body.addEventListener('touchmove', e => {
   touchEnd = e.targetTouches[0].clientX;
-  /*console.log(touchEnd)*/
+
 });
 
 body.addEventListener('touchend', e => {
@@ -119,3 +203,11 @@ body.addEventListener('touchend', e => {
     closesidebar.classList.remove('open');
   }
 });
+
+*/
+/*************Responssive*************/
+
+
+
+
+

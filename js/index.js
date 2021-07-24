6d67425e2c81f1831782fab32927db6dde4e7f74
jprex59 <jean59paris@orange.fr>
2021-07-24 12:52:53 +0200

    plus epurée

--- js/index.js
+++ js/index.js
@@ -24,18 +24,22 @@ document.addEventListener("DOMContentLoaded", () => {
 // recuperer les elements
 let SectionName = document.getElementById("SectionName");
 let contact = document.getElementById("contact");
+let informations = document.getElementById("informations");
 let presentationContainer = document.querySelector(".presentation-container");
 
 /*****/
 let sectionNameTop = SectionName.offsetTop;
+let informationsTop = informations.offsetTop;
 let contactTop = contact.offsetTop;
 
 // faire la fonction
 /*****Plus tard mettre dans un tableau*****/
 function Section() {
 	console.log(window.pageYOffset);
-	if (window.pageYOffset < contactTop) {
+	if (window.pageYOffset < informationsTop) {
 		SectionName.innerHTML = "Mon Portfolio";
+	} else if (window.pageYOffset > informationsTop) {
+		SectionName.innerHTML = "A Propos";
 	} else if (window.pageYOffset > contactTop) {
 		SectionName.innerHTML = "Contact";
 	}

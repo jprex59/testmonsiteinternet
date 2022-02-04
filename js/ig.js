89ecf3491be0786f438136383c5334a13df33f6f
jprex59 <jean59paris@orange.fr>
2022-02-04 22:53:21 +0100

    cover content infos 1/2 +

--- js/ig.js
+++ js/ig.js
@@ -5,19 +5,19 @@ const cover = document.querySelectorAll(".cover");
 var ouverture = [];
 
 for (let i = 0; i < more.length; i++) {
+	ouverture[[i, 0]] = 0;
 	ouverture[[i, 1]] = 0;
-	ouverture[[i, 2]] = 0;
 
 	more[i].addEventListener("click", () => {
-		ouverture[[i, 1]] = ouverture[[i, 1]] + 1;
-		ouverture[[i, 2]] = ouverture[[i, 1]] % 2;
+		ouverture[[i, 0]] = ouverture[[i, 0]] + 1;
+		ouverture[[i, 1]] = ouverture[[i, 0]] % 2;
 
-		if (ouverture[[i, 2]] == 1) {
+		if (ouverture[[i, 1]] == 1) {
 			cover[i].classList.add("is-active");
 			arrow[i].classList.add("arrow_is-active");
 			more[i].classList.add("more-is-active");
 			more[i].style.transition = "background-color 0.7s";
-		} else if (ouverture[[i, 2]] == 0) {
+		} else if (ouverture[[i, 1]] == 0) {
 			cover[i].classList.remove("is-active");
 			arrow[i].classList.remove("arrow_is-active");
 			more[i].classList.remove("more-is-active");
